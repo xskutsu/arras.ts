8a7d5f190cd22fdb247a6d13a7083342d7b38b10
xskutsu <xsku@protonmail.com>
2026-03-09 21:58:13 -0500

    Run biome linter

--- src/client/lib/color.js
+++ src/client/lib/color.js
@@ -37,26 +37,26 @@
     exports.dgreen =  "#495E07";
     */
 
-exports.teal    = '#70D1CA'; // 0 
-exports.lgreen  = '#B9E87E'; // 1 
-exports.orange  = '#E7896D'; // 2 
-exports.yellow  = '#FDF380'; // 3 
-exports.lavender ='#B58EFD'; // 4 
-exports.pink    = '#EF99C3'; // 5 
-exports.vlgrey  = '#EAEAEA'; // 6 
-exports.lgrey   = '#D8D6D6'; // 7 
-exports.guiwhite ='#FFFFFF'; // 8 
-exports.black   = '#484848'; // 9 
+exports.teal = "#70D1CA"; // 0
+exports.lgreen = "#B9E87E"; // 1
+exports.orange = "#E7896D"; // 2
+exports.yellow = "#FDF380"; // 3
+exports.lavender = "#B58EFD"; // 4
+exports.pink = "#EF99C3"; // 5
+exports.vlgrey = "#EAEAEA"; // 6
+exports.lgrey = "#D8D6D6"; // 7
+exports.guiwhite = "#FFFFFF"; // 8
+exports.black = "#484848"; // 9
 
-exports.blue    = '#3CA4CB'; // 10 
-exports.green   = '#8ABC3F'; // 11
-exports.red     = '#D83848'; // 12 
-exports.gold    = '#EFC74B'; // 13 
-exports.purple  = '#8D6ADF'; // 14 
-exports.magenta = '#CC669C'; // 15 
-exports.grey    = '#B5B5B9'; // 16 
-exports.dgrey   = '#999797'; // 17 
-exports.white   = '#DDE6EB'; // 18 
-exports.guiblack ='#000000'; // 19 
+exports.blue = "#3CA4CB"; // 10
+exports.green = "#8ABC3F"; // 11
+exports.red = "#D83848"; // 12
+exports.gold = "#EFC74B"; // 13
+exports.purple = "#8D6ADF"; // 14
+exports.magenta = "#CC669C"; // 15
+exports.grey = "#B5B5B9"; // 16
+exports.dgrey = "#999797"; // 17
+exports.white = "#DDE6EB"; // 18
+exports.guiblack = "#000000"; // 19
 
 exports.paletteSize = 10;
--- src/server/lib/util.js
+++ src/server/lib/util.js
@@ -1,25 +1,16 @@
 /* jslint node: true */
 
-'use strict';
+var cfg = require("../../../config.json");
 
-var cfg = require('../../../config.json');
+exports.addArticle = (string) =>
+	/[aeiouAEIOU]/.test(string[0]) ? "an " + string : "a " + string;
 
-exports.addArticle = function(string) {
-    return (/[aeiouAEIOU]/.test(string[0])) ? 'an ' + string : 'a ' + string;
-};
+exports.getDistance = (p1, p2) =>
+	Math.sqrt((p2.x - p1.x) ** 2 + (p2.y - p1.y) ** 2);
 
-exports.getDistance = function (p1, p2) {
-    return Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2));
-};
+exports.getDirection = (p1, p2) => Math.atan2(p2.y - p1.y, p2.x - p1.x);
 
-exports.getDirection = function (p1, p2) {
-    return Math.atan2(p2.y - p1.y, p2.x - p1.x);
-};
-
-exports.clamp = function(value, min, max) {
-    return Math.min(Math.max(value, min), max);
-};
-
+exports.clamp = (value, min, max) => Math.min(Math.max(value, min), max);
 
 /*exports.angleDifference = function(a1, a2) {
     let diff1 = a2 - a1;
@@ -40,18 +31,16 @@ exports.clamp = function(value, min, max) {
     if (Math.abs(diff1) <= Math.abs(diff2)) { return diff1; }
     if (Math.abs(diff2) <= Math.abs(diff1)) { return diff2; }
 };*/
-exports.angleDifference = (() => {    
-    let mod = function(a, n) {
-        return (a % n + n) % n;
-    };
-    return (sourceA, targetA) => { 
-        let a = targetA - sourceA;
-        return mod(a + Math.PI, 2*Math.PI) - Math.PI;
-    };
+exports.angleDifference = (() => {
+	const mod = (a, n) => ((a % n) + n) % n;
+	return (sourceA, targetA) => {
+		const a = targetA - sourceA;
+		return mod(a + Math.PI, 2 * Math.PI) - Math.PI;
+	};
 })();
 
 exports.loopSmooth = (angle, desired, slowness) => {
-    return exports.angleDifference(angle, desired)/slowness;
+	return exports.angleDifference(angle, desired) / slowness;
 };
 
 /*exports.loopClamp = function(angle, min, max) {
@@ -63,7 +52,6 @@ exports.loopSmooth = (angle, desired, slowness) => {
     if (angle - min < 0) return min;
     return angle;
 };*/
-
 
 /*exports.pointInArc = function(point, givenAngle, allowedDifference) {
     let len = Math.sqrt(point.x * point.x + point.y * point.y);
@@ -80,83 +68,98 @@ exports.loopSmooth = (angle, desired, slowness) => {
 };*/
 
 exports.deepClone = (obj, hash = new WeakMap()) => {
-    let result;
-    // Do not try to clone primitives or functions
-    if (Object(obj) !== obj || obj instanceof Function) return obj;
-    if (hash.has(obj)) return hash.get(obj); // Cyclic reference
-    try { // Try to run constructor (without arguments, as we don't know them)
-        result = new obj.constructor();
-    } catch(e) { // Constructor failed, create object without running the constructor
-        result = Object.create(Object.getPrototypeOf(obj));
-    }
-    // Optional: support for some standard constructors (extend as desired)
-    if (obj instanceof Map)
-        Array.from(obj, ([key, val]) => result.set(exports.deepClone(key, hash), 
-                                                   exports.deepClone(val, hash)) );
-    else if (obj instanceof Set)
-        Array.from(obj, (key) => result.add(exports.deepClone(key, hash)) );
-    // Register in hash    
-    hash.set(obj, result);
-    // Clone and assign enumerable own properties recursively
-    return Object.assign(result, ...Object.keys(obj).map (
-        key => ({ [key]: exports.deepClone(obj[key], hash) }) ));
+	let result;
+	// Do not try to clone primitives or functions
+	if (Object(obj) !== obj || obj instanceof Function) return obj;
+	if (hash.has(obj)) return hash.get(obj); // Cyclic reference
+	try {
+		// Try to run constructor (without arguments, as we don't know them)
+		result = new obj.constructor();
+	} catch (e) {
+		// Constructor failed, create object without running the constructor
+		result = Object.create(Object.getPrototypeOf(obj));
+	}
+	// Optional: support for some standard constructors (extend as desired)
+	if (obj instanceof Map)
+		Array.from(obj, ([key, val]) =>
+			result.set(
+				exports.deepClone(key, hash),
+				exports.deepClone(val, hash),
+			),
+		);
+	else if (obj instanceof Set)
+		Array.from(obj, (key) => result.add(exports.deepClone(key, hash)));
+	// Register in hash
+	hash.set(obj, result);
+	// Clone and assign enumerable own properties recursively
+	return Object.assign(
+		result,
+		...Object.keys(obj).map((key) => ({
+			[key]: exports.deepClone(obj[key], hash),
+		})),
+	);
 };
 
-exports.averageArray = arr => {
-    if (!arr.length) return 0;    
-    var sum = arr.reduce((a, b) => { return a + b; });
-    return sum / arr.length;
+exports.averageArray = (arr) => {
+	if (!arr.length) return 0;
+	var sum = arr.reduce((a, b) => {
+		return a + b;
+	});
+	return sum / arr.length;
 };
 
-exports.sumArray = arr => {
-    if (!arr.length) return 0;    
-    var sum = arr.reduce((a, b) => { return a + b; });
-    return sum;
+exports.sumArray = (arr) => {
+	if (!arr.length) return 0;
+	var sum = arr.reduce((a, b) => {
+		return a + b;
+	});
+	return sum;
 };
 
-
-exports.signedSqrt = x => {
-    return Math.sign(x) * Math.sqrt(Math.abs(x));
+exports.signedSqrt = (x) => {
+	return Math.sign(x) * Math.sqrt(Math.abs(x));
 };
 
-exports.getJackpot = x => {
-    return (x > 26300 * 1.5) ? Math.pow(x - 26300, 0.85) + 26300 : x / 1.5;
+exports.getJackpot = (x) => {
+	return x > 26300 * 1.5 ? (x - 26300) ** 0.85 + 26300 : x / 1.5;
 };
 
 exports.serverStartTime = Date.now();
 // Get a better logging function
 exports.time = () => {
-    return Date.now() - exports.serverStartTime;
+	return Date.now() - exports.serverStartTime;
 };
 
 // create a custom timestamp format for log statements
-const SimpleNodeLogger = require('simple-node-logger'),
-    logger = SimpleNodeLogger.createRollingFileLogger({
-        logDirectory: __dirname + '/../../../logs',
-        fileNamePattern:'diep2-<DATE>.log',
-        dateFormat:'YYYY-MM-DD',
-        level: 'warn',
-    });
-exports.log = text => {
-    console.log('[' + (exports.time()/1000).toFixed(3) + ']: ' + text);
-    logger.info(text);
+const SimpleNodeLogger = require("simple-node-logger"),
+	logger = SimpleNodeLogger.createRollingFileLogger({
+		logDirectory: __dirname + "/../../../logs",
+		fileNamePattern: "diep2-<DATE>.log",
+		dateFormat: "YYYY-MM-DD",
+		level: "warn",
+	});
+exports.log = (text) => {
+	console.log("[" + (exports.time() / 1000).toFixed(3) + "]: " + text);
+	logger.info(text);
 };
-exports.warn = text => {
-    console.log('[' + (exports.time()/1000).toFixed(3) + ']: ' + '[WARNING] ' + text);
-    logger.warn(text);
+exports.warn = (text) => {
+	console.log(
+		"[" + (exports.time() / 1000).toFixed(3) + "]: " + "[WARNING] " + text,
+	);
+	logger.warn(text);
 };
-exports.error = text => {
-    console.log(text);
-    logger.error(text);
+exports.error = (text) => {
+	console.log(text);
+	logger.error(text);
 };
-exports.remove = (array, index) => {    
-    // there is more than one object in the container
-    if(index === array.length - 1){
-        // special case if the obj is the newest in the container
-        return array.pop();
-    } else {
-        let o = array[index];
-        array[index] = array.pop();
-        return o;
-    }
+exports.remove = (array, index) => {
+	// there is more than one object in the container
+	if (index === array.length - 1) {
+		// special case if the obj is the newest in the container
+		return array.pop();
+	} else {
+		const o = array[index];
+		array[index] = array.pop();
+		return o;
+	}
 };
